e8880cd853b98619bdfa209bc09b051737297b26
Emilka <zaleska_emilia@wp.pl>
2019-01-14 21:10:41 +0100

    GUI interface - testing various cases

--- interfejs.py
+++ interfejs.py
@@ -19,6 +19,16 @@ def test_slownik():
     wystapienia = {"Portal1": "20.01.2018", "Portal2": "21.01.2018"}
     print(wystapienia)
 
+def test_z_argumentem(str):
+    print(str)
+
+def test_przekazania_zmiennej(arg):
+    if arg == "Nazwisko1":
+        print("Portal1 - 15.01.2019")
+    elif arg == "Nazwisko2":
+        print("Portal3 - 20.01.2019")
+    else:
+        print("Nie znaleziono")
 
 # Creating main window
 main_window = tk.Tk()
@@ -40,7 +50,7 @@ sys.stdout.write = redirector
 label_1 = tk.Label(topFrame, text="Informacje o nowościach")
 label_1.grid(row=0, columnspan=2)
 
-button_1 = tk.Button(topFrame, text="Nowe słowa", bg="black", fg="red", command=test_lista)
+button_1 = tk.Button(topFrame, text="Nowe słowa", bg="black", fg="red", command=lambda: test_z_argumentem("Testowo"))
 button_1.grid(row=1, columnspan=2)
 
 button_2 = tk.Button(topFrame, text="Słowa na innych portalach", bg="black", fg="red", command=test_lista2)
@@ -49,10 +59,11 @@ button_2.grid(row=2, columnspan=2)
 label_2 = tk.Label(topFrame, text="\nSzukaj wystąpień")
 label_2.grid(row=3, columnspan=2)
 
-entry_1 = tk.Entry(topFrame)
+nazwisko = tk.StringVar()
+entry_1 = tk.Entry(topFrame, textvariable=nazwisko)
 entry_1.grid(row=4, column=0)
 
-button_3 = tk.Button(topFrame, text="Szukaj", bg="black", fg="red", command=test_slownik)
+button_3 = tk.Button(topFrame, text="Szukaj", bg="black", fg="red", command=lambda: test_przekazania_zmiennej(nazwisko.get()))
 button_3.grid(row=4, column=1)
 
 # Bottom frame - label and textbox
